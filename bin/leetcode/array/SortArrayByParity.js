f29157d46e406a5e0fb9fe7fd5ce0f122412cdde
yongseondu <weed0124@naver.com>
2020-07-05 13:46:09 +0900

    LeetCode 905. Sort Array By Parity

---
+++ bin/leetcode/array/SortArrayByParity.js
@@ -0,0 +1,17 @@
+/**
+ * LeetCode 905. Sort Array By Parity.
+ * 
+ * @param {number[]} A
+ * @return {number[]}
+ */
+var sortArrayByParity = function(A) {
+	var evenNum = [], oddNum = [];
+	A.map(function(a) {
+		if (a % 2 == 0) {
+			evenNum.push(a);
+		} else {
+			oddNum.push(a);
+		}
+	});
+	return evenNum.concat(oddNum);
+};
