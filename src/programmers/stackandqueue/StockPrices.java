0c5013b8b97480b8fdd26676f5f89a4efe13a59c
yongseondu <weed0124@naver.com>
2020-10-20 20:33:14 +0900

    Programmers - 주식가격

---
+++ src/programmers/stackandqueue/StockPrices.java
@@ -0,0 +1,47 @@
+package programmers.stackandqueue;
+
+public class StockPrices {
+	
+	public static void main(String[] args)
+	{
+		StockPrices item = new StockPrices();
+		int[] testCase1 = {1, 2, 3, 2, 3};
+		item.printArray(item.solution(testCase1));
+	}
+	
+	public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        for (int i = 0; i < prices.length; i++)
+        {
+        	for (int j = i + 1; j < prices.length; j++)
+        	{
+        		if (prices[i] <= prices[j])
+        		{
+        			answer[i] = j - i;
+        		}
+        		else
+        		{
+        			answer[i] = j - i;
+        			break;
+        		}
+        	}
+        }
+        return answer;
+    }
+	
+	public void printArray(int[] array)
+	{
+		for (int i = 0; i < array.length; i++)
+		{
+			if (i != array.length - 1)
+			{
+				System.out.print(array[i] + ", ");
+			}
+			else
+			{
+				System.out.print(array[i]);
+			}
+		}
+		System.out.println();
+	}
+}
