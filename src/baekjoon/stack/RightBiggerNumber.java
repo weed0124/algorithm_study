bbb4f6e4936485801ba8566a6e6fc642a4782c49
weed0124 <weed0124@naver.com>
2023-09-14 22:16:52 +0900

    Stack 17298번 오큰수 문제 주석 추가

--- src/baekjoon/stack/RightBiggerNumber.java
+++ src/baekjoon/stack/RightBiggerNumber.java
@@ -16,6 +16,9 @@ public class RightBiggerNumber {
 	 * 오큰수가 인접해있지 않는 경우 또는 없는 경우에 대해서 처리가 안되는 문제가 있어
 	 * 입력값과 동일한 데이터를 임시로 저장해놓을 배열과 결과값을 출력 시켜줄 배열을 선언 후
 	 * 마지막에 오큰수가 없는 값들을 -1로 치환해준 후 출력 처리
+	 * 
+	 * 스택은 오큰수를 찾지못한 수들의 인덱스를 저장해놓고 다음 숫자와 비교하며 오큰수가 맞다면 스택에서 제거한다.
+	 * 위와 같이 처리 시 오큰수를 찾지 못한 경우에 대한 처리가 안되어 마지막으로 스택에 남아있는 인덱스에 대한 값들을 -1로 치환해주면 된다.
 	 */
 	public static void main(String[] agrs) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
