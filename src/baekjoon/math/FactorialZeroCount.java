a0f0ccca94eb0ded2eea210b47ea2a9f242e25ed
weed0124 <weed0124@naver.com>
2023-09-18 12:49:55 +0900

    Math 1676번 팩토리얼 0의 개수 수정

--- src/baekjoon/math/FactorialZeroCount.java
+++ src/baekjoon/math/FactorialZeroCount.java
@@ -13,15 +13,8 @@ public class FactorialZeroCount {
 		int result = 0;
 		
 		// 0이 나오는 경우는 2 * 5의 경우만 있고, 2는 5의 개수보다 적으니 5의 개수만 체크한다
-		// 25는 5 * 5로 두번이나오니 추가로 세어준다.
-		for (int i = 2; i <= n; i++) {
-			if (i % 5 == 0) {
-				result += 1;
-			}
-			
-			if (i % 25 == 0) {
-				result += 1;
-			}
+		for (int i = 5; i <= n; i *= 5) {
+			result += n / i;
 		}
 		
 		System.out.println(result);
