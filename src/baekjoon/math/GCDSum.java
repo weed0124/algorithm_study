d94733a30e4e2dfc05ef77178a754ea3bb3dddc5
weed0124 <weed0124@naver.com>
2023-09-19 10:41:28 +0900

    Math 9613번 GCD합

---
+++ src/baekjoon/math/GCDSum.java
@@ -0,0 +1,42 @@
+package baekjoon.math;
+
+import java.util.Scanner;
+
+/**
+ * https://www.acmicpc.net/problem/9613
+ * GCD 합
+ */
+public class GCDSum {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		while (n-- > 0) {
+			int sum = 0;
+			
+			int m = sc.nextInt();
+			int[] arr = new int[m];
+			
+			for (int i = 0; i < arr.length; i++) {
+				arr[i] = sc.nextInt();
+			}
+			
+			for (int i = 0; i < m - 1; i++) {
+				for (int j = i + 1; j < m; j++) {
+					sum += gcd(arr[i], arr[j]);
+				}
+			}
+			
+			System.out.println(sum);
+		}
+		
+	}
+	
+	static int gcd(int a, int b) {
+		if (b == 0) {
+			return a;
+		} else {
+			return gcd(b, a % b);
+		}
+	}
+}
