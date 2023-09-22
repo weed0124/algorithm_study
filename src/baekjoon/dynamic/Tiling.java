64a6aef27623be2572c7a5c01ccb4f271acfc1fa
weed0124 <weed0124@naver.com>
2023-09-22 12:15:05 +0900

    Dynamic 11726번 2*n타일링

---
+++ src/baekjoon/dynamic/Tiling.java
@@ -0,0 +1,42 @@
+package baekjoon.dynamic;
+
+import java.util.Scanner;
+
+/**
+ * https://www.acmicpc.net/problem/11726
+ * 2 * n 타일링
+ */
+public class Tiling {
+	public static int[] d;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(); // 1 <= n <= 1000
+		
+		d = new int[n + 1];
+		System.out.println(solution(n));
+	}
+	
+	// 2*n크기의 직사각형을 채우는 방법의 수를 d[n]이라 할때 
+	static int solution(int n) {
+		
+		// d[1] = 1
+		if (n == 1) {
+			return 1;
+		}
+		
+		// d[2] = 2
+		if (n == 2) {
+			return 2;
+		}
+		
+		// Memoization
+		if (d[n] > 0) {
+			return d[n];
+		}
+		
+		d[n] = solution(n - 1) + solution(n - 2);
+		
+		return d[n] % 10007;
+	}
+}
