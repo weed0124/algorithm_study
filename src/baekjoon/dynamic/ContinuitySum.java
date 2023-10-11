ed3b92360c4f39fcd5d15b5d33c10572d1db54d9
weed0124 <weed0124@naver.com>
2023-10-11 09:17:22 +0900

    소스 수정

--- src/baekjoon/dynamic/ContinuitySum.java
+++ src/baekjoon/dynamic/ContinuitySum.java
@@ -12,25 +12,21 @@ public class ContinuitySum {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int[] a = new int[n];
+		int[] a = new int[n], d = new int[n];
 		for (int i = 0; i < n; i++) {
 			a[i] = sc.nextInt();
 		}
 		
-		int[] d = new int[n];
 		for (int i = 0; i < n; i++) {
 			d[i] = a[i];
-			if (i == 0) {
-				continue;
-			}
 			
-			if (a[i] + d[i - 1] > d[i]) {
+			if (i > 0 && d[i] < d[i - 1] + a[i]) {
 				d[i] = d[i - 1] + a[i]; 
 			}
 		}
 		
 		int result = d[0];
-		for (int i = 0; i < n; i++) {
+		for (int i = 1; i < n; i++) {
 			if (result < d[i]) {
 				result = d[i];
 			}
