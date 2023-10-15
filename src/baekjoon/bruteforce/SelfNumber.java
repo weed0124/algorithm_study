e7fe3e768fc0b8a504afb4bebd3ddbe34e9a4ba1
weed0124 <weed0124@naver.com>
2023-10-15 20:03:02 +0900

    BruteForce 4673번 셀프 넘버 - 코드 수정

--- src/baekjoon/bruteforce/SelfNumber.java
+++ src/baekjoon/bruteforce/SelfNumber.java
@@ -6,13 +6,12 @@ package baekjoon.bruteforce;
  * 
  * 문제범위는 최대 10000개 이하로 모든 경우의수 체크
  * 두가지 방법으로 풀이가능
- * 1. 각 문자열로 잘라서 처리 -> 메모리 14864KB 시간 136ms
+ * 1. 각 문자열로 잘라서 처리 -> 메모리 14728KB 시간 148ms
  * 2. 해당 값을 10으로 나눈 나머지값을 반복하여 처리 -> 메모리 13884KB 시간 116ms
  * 
  * 시간차이 및 코드 길이는 크게 안남 편한대로 처리하면 될듯
  */
 public class SelfNumber {
-//	static int[] check = new int[10000];
 	static boolean[] check = new boolean[10000];
 	
 	public static int d(int n) {
@@ -26,7 +25,8 @@ public class SelfNumber {
 	
 	public static void main(String[] args) {
 		StringBuilder sb = new StringBuilder();
-		
+		/*
+		// 2. 숫자를 10으로 나눈 나머지값으로 반복하여 처리
 		for (int i = 1; i < 10000; i++) {
 			int construct = d(i);
 			if (construct < 10000) {
@@ -39,12 +39,9 @@ public class SelfNumber {
 		}
 		
 		System.out.println(sb);
+		*/
 		
-		/* 1. 숫자를 자리수별로 문자열로 잘라서 처리 
-		for (int i = 1; i < 10000; i++) {
-			check[i] = i;
-		}
-		
+		// 1. 숫자를 자리수별로 문자열로 잘라서 처리 
 		for (int i = 1; i < 10000; i++) {
 			char[] c = String.valueOf(i).toCharArray();
 			int sum = i;
@@ -52,16 +49,16 @@ public class SelfNumber {
 				sum += Character.digit(c[j], 10);
 			}
 			if (sum < 10000) {
-				check[sum] = -1;
+				check[sum] = true;
 			}
 		}
 		
 		for (int i = 1; i < 10000; i++) {
-			if (check[i] != -1) {
+			if (!check[i]) {
 				sb.append(i).append("\n");
 			}
 		}
 		System.out.println(sb);
-		*/
+		
 	}
 }
